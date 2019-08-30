3acba3a999ed1ee3bc6c66e1706a8d6f47b0ba73
Danny Lai <tocalai@gmail.com>
2019-08-30 10:51:25 +0800

    Initial commit

---
+++ SignalR.Lab.Web/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SignalR.Lab.Web.Services;
+
+namespace SignalR.Lab.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+
+                try
+                {
+                    var serviceContext = services.GetRequiredService<EventReceiverService>();
+
+                    serviceContext.StartReceive();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception occured, message: {ex.Message}, stacktrace: {ex.StackTrace}");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
